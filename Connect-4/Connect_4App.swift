5f5d5c5aa99f928d51126e161b52a33ea91e5f76
Paweł Czuchryta <pawel.czuchryta@sap.com>
2024-01-05 17:16:39 +0100

    Initial Commit

---
+++ Connect-4/Connect_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect_4App.swift
+//  Connect-4
+//
+//  Created by Czuchryta, Pawel on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Connect_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
